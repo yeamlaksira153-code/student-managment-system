03b000129d0018cff77676e0a507ef76d883848f
yeamlaksira demeke <yeamlaksira153@gmail.com>
2026-07-11 13:11:25 +0200

    search student by id

--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -8,17 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner (System.in);
 StudentService service=new StudentService();
-Student student1= new Student(
-	1234 ,"jhon",22,"computer science",88.2
-		);
-		
-Student student2 = new Student(
-	2345 ,"lily",21,"software development",99);
-			
-		service.addstudent(student1);
-		service.addstudent(student2);
-		service.displaystudents();
-		
+
 		boolean running=true;
 		while(running) {
 			System.out.println("\n==============================");
@@ -26,7 +16,8 @@ Student student2 = new Student(
 	        System.out.println("==============================");
 	        System.out.println("1. Add Student");
 	        System.out.println("2. Display Students");
-	        System.out.println("3. Exit");
+	        System.out.println("3. search student");
+	        System.out.println("4. Exit");
 	        System.out.print("Choose an option: ");
 	        
 	        int choice=sc.nextInt();
@@ -51,28 +42,60 @@ Student student2 = new Student(
     System.out.print("Enter Name: ");
     String name = sc.nextLine();
 
+    int age;
+    while(true) {
     System.out.print("Enter Age: ");
-    int age = sc.nextInt();
+     age = sc.nextInt();
+     
+     if(age>=16 && age<=100) {
+    	 break;
+    	 
+     }System.out.println("age must be between 16 and 100");
     sc.nextLine();
-
+    
+    }
     System.out.print("Enter Course: ");
     String course = sc.nextLine();
 
+    double averageMark;
+    while(true) {
     System.out.print("Enter Average Mark: ");
-    double averageMark = sc.nextDouble();
+   averageMark = sc.nextDouble();
     sc.nextLine();
-	
+    
+    if(averageMark>=0 && averageMark<=100) {}
+    break;
+    }System.out.println("average mark must be between 0 and 100");
     Student student=new Student (
     	id,name,age,course,averageMark	
     		);
-    service.addstudent(student);
+    service.addStudent(student);
     System.out.println("student added successfully");
     
 	break;
 	case 2: System.out.println("list of students ");
-	service.displaystudents();
+	service.displayStudents();
 	break;
-	case 3: System.out.println("exiting the system ");
+	case 3:
+		System.out.println("enter student Id");
+		 int searchId=sc.nextInt();
+		 sc.nextLine();
+		
+		 Student foundStudent=service.findStudentById(searchId);
+		 if (foundStudent != null) {
+
+		        System.out.println("\nStudent Found");
+		        System.out.println(foundStudent);
+
+		    } else {
+
+		        System.out.println("Student not found.");
+
+		    }
+
+		    break;
+		
+	case 4: System.out.println("exiting the system ");
 	running=false;
 	break;
 	default: System.out.println("invalid choice");
